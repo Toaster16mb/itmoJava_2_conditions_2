3b66a400e6933d35eb629b0ad7d734a34a356247
Stepan Alekseev <toaster16mb@gmail.com>
2017-03-15 20:10:06 +0300

    nvm

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         double m,n;
         n = 10.5;
         m = 8.6;
+        // выводим число из m и n, которое ближе к 10
         System.out.println((Math.abs(10.0 - m) < Math.abs(10.0 - n)) ? m : n);
     }
 }
