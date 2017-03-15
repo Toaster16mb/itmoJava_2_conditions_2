565830bcc8c9960f280928bae6c5b1945fd12f76
Stepan Alekseev <toaster16mb@gmail.com>
2017-03-15 19:44:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double m,n;
+        n = 10.5;
+        m = 8.6;
+        System.out.println((Math.abs(10.0 - m) < Math.abs(10.0 - n)) ? m : n);
+    }
+}
